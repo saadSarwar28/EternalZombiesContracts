357bbcfb6c122c94e70910e16d1c758b0918ebaf
saadSarwar28 <saad_sarwar22@hotmail.com>
2022-08-12 00:42:19 +0500

    deployment script updated

--- migrations/2_eternalZombies_nft.js
+++ migrations/2_eternalZombies_nft.js
@@ -1,16 +1,35 @@
-// const MainStreetNft = artifacts.require("MainStreetNFT");
-// const Staker = artifacts.require("EternalZombiesStaker");
-// const simpleNFT = artifacts.require("simpleNFT")
-// const EternalZombies = artifacts.require("EternalZombies");
-// const maxNfts = 5000
-// const salePrice = '200000000000000000'
-// const staker = '0xc644b4bb9f171c0d0f00f46e73562ab501a1edce'
-// const previousContract = '0xAc760d623F0181FDa420271746b7AF39e88F934a'
+const EternalZombies = artifacts.require('EternalZombies')
+const Staker = artifacts.require('EternalZombiesStaker')
+const distributor = artifacts.require('EternalZombiesDistributor')
+// Mainnet
+const wrappedBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
+const zmbe = '0x50ba8bf9e34f0f83f96a340387d1d3888ba4b3b5'
+const pancakeRouter = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+const minter = ''
+const distributor = ''
+const drFrankenstein = '0x590Ea7699A4E9EaD728F975efC573f6E34a5dC7B'
+const pancakeLP = '0x4DBaf6479F0Afa9f03C2A7D611151Fa5b53ECdC8'
+const reStakingPercentage = 10
+const devPercentage = 5;
+
 
 module.exports = async function (deployer) {
+
     // await deployer.deploy(EternalZombies, maxNfts, salePrice, staker);
-    // const test = await EternalZombies.deployed();
-    // await deployer.deploy(simpleNFT);
-    // const test = await simpleNFT.deployed();
-    // await deployer.deploy(MainStreetNft, maxNfts, salePrice, previousContract, staker);
+    // await EternalZombies.deployed();
+
+
+    await deployer.deploy(staker,
+        wrappedBNB,
+        zmbe,
+        pancakeRouter,
+        minter,
+        distributor,
+        drFrankenstein,
+        pancakeLP,
+        reStakingPercentage,
+        devPercentage
+    )
+
+
 };
